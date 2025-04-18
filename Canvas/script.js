a87e763cc2cd20ba440f4c7b302632aa3f4063a6
ぺゅ～ <jinyexingyang@gmail.com>
2025-04-19 01:02:01 +0900

    Update script.js

--- Canvas/script.js
+++ Canvas/script.js
@@ -1,113 +1,51 @@
-// 各要素を取得
-const textarea = document.getElementById("textarea");
-const canvas = document.getElementById("canvas");
-const ctx = canvas.getContext("2d");
+const teb_ctrl = Array.from(document.getElementsByClassName(`tabctrl`));
+const tab_arr = Array.from(document.getElementsByClassName(`tabs`));
+const input_num_arr = Array.from(document.getElementsByClassName(`inputnum`));
+const textarea = document.getElementById(`textarea`);
+const canvas = document.getElementById(`canvas`);
+const log_area = document.getElementById(`log_area`);
+const resize_bar = document.getElementById(`resizebar`);
+const ui_div = document.getElementById(`UIdiv`);
+const ctx = canvas.getContext(`2d`);
+const w_w = window.innerWidth;
+const w_h = window.innerHeight;
+const min_lwidth = 100;
+const min_rwidth = w_w - (w_h - 50) - 7;
+const lsystem = [`Axiom:`, `->`, `;`, `F`, `+`, `-`, `[`, `]`];
+const init_prm = [0, 0, 0, 60, 0.04, 10, 5];
+let is_dragg = false;
+let keep_list, canvas_size;
+textarea.addEventListener(`keyup`, main);
 
-// グローバル変数
-let canvasSize;
-
-//パラメータ
-const iteration = 5; // 変換ルールの適用回数
-const graphsize = 10; // 描画するグラフの最大x,y
-const lineLength = 0.04; // 一度の命令で引く線の長さ
-const angle = Math.PI / 3; // 一度の命令で曲がる角度
-let currentPoint = [0, 0]; // 現在の座標
-let currentAngle = 0; // 現在の向いている方向
-let keep; // 命令[で保存するための変数
-
-// このツールで利用可能なLsystemの命令
-const Lsystem = ["Axiom:", "->", ";", "F", "+", "-", "[", "]"];
-
-// リサイズ・ロード時の処理(キャンバスの初期化)
-function resizeCanvas() {
-    const rect = canvas.getBoundingClientRect();
-    canvasSize = rect.width;
-    canvas.width = canvasSize;
-    canvas.height = canvasSize;
-}
-
-function resetCanvas() {
-    currentPoint = [0, 0];
-    currentAngle = 0;
-    keep = { point: undefined, angle: undefined };
-    ctx.clearRect(0, 0, canvasSize, canvasSize);
-}
-
-window.addEventListener("resize", resizeCanvas);
-window.addEventListener("load", resizeCanvas);
-
-// テキストエリア(Lsystemの命令欄)への入力内容の処理
-textarea.addEventListener("keyup", (event) => {
-    const instructions = event.target.value;
-    let isAxiom = undefined;
-    let isrule = true;
-    let rule = [];
-    let tmp = [];
-    let axiom = [];
-    // Lsystemの文字列の処理(axiom:,->,;)
-    for (let element of find(instructions)) {
-        if (isAxiom !== false) {
-            if (element === "Axiom:") {
-                isAxiom = true; continue;
-            };
-            if (element === ";" && isAxiom) isAxiom = false;
-            if (isAxiom) axiom.push(element);
-        } else {
-            if (element === "->" || ";" === element) {
-                if (element === (isrule ? "->" : ";")) {
-                    isrule = !isrule;
-                    rule.push(tmp);
-                };
-                tmp = [];
-            } else {
-                tmp.push(element);
-            }
-        }
+resizebar.addEventListener(`mousedown`, (ev) => {
+    is_dragg = true;
+    document.body.style.cursor = `col-resize`;
+});
+document.addEventListener(`mouseup`, () => {
+    is_dragg = false;
+    document.body.style.cursor = `default`;
+});
+document.addEventListener(`mousemove`, (ev) => {
+    if (!is_dragg) return;
+    const l = ev.clientX;
+    const r = w_w - l - 7;
+    if (min_lwidth < l && min_rwidth < r) {
+        ui_div.style.gridTemplateColumns = `${l}px 7px ${r}px`;
     };
-    for (let i = 0; i < iteration; i++) {
-        for (let j = 0; j < (rule.length - rule.length % 2) / 2; j++) {
-            axiom = replace(axiom, rule[2 * j], rule[2 * j + 1]);
-        }
-    }
-    const drawrule = axiom;
-    resetCanvas();
-    drawrule.forEach(rule => {
-        switch (rule) {
-            case "F": L.F(); break;
-            case "+": L.plus(); break;
-            case "-": L.minus(); break;
-            case "[": L.sbS(); break;
-            case "]": L.sbF(); break;
-        }
-    })
 });
 
-const resizebar = document.getElementById("resizebar");
-let isDragg = false;
-resizebar.addEventListener("mousedown", (event) => {
-    isDragg = true;
-    document.body.style.cursor = "col-resize";
-});
-document.addEventListener("mouseup", () => {
-    isDragg = false;
-    document.body.style.cursor = "default";
-});
-document.addEventListener("mousemove", (event) => {
-    if (!isDragg) return;
-    const left = event.clientX;
-    const right = window.innerWidth - left - 7;
-    document.getElementById("UIdiv").style.gridTemplateColumns = `${left}px 7px ${right}px`;
-});
+function var_init() {
+    input_num_arr.forEach((el, i) => el.value = init_prm[i])
+}
 
-// 配列の一部分を全て置き換える関数
-function replace(arr, target, replacement) {
-    const targetLength = target.length;
+function replace(arr, target, replace) {
+    const trg_l = target.length;
     let i = 0;
-    while (i <= arr.length - targetLength) {
-        const slice = arr.slice(i, i + targetLength);
-        if (slice.length === target.length && slice.every((val, i) => val === target[i])) {
-            arr.splice(i, targetLength, ...replacement);
-            i += replacement.length;
+    while (i <= arr.length - trg_l) {
+        const slice = arr.slice(i, i + trg_l);
+        if (slice.length === trg_l && slice.every((val, i) => val === target[i])) {
+            arr.splice(i, trg_l, ...replace);
+            i += replace.length;
         } else {
             i++;
         }
@@ -115,76 +53,163 @@ function replace(arr, target, replacement) {
     return arr;
 }
 
-// 引数checkwordをLsystemの命令で分解する
-function find(checkword) {
+function find(word) {
     const result = [];
     let i = 0;
-    while (i < checkword.length) {
-        const matches = Lsystem.filter(key => checkword.slice(i).startsWith(key)); // 文字が一致するものを絞り込
-        if (matches.length > 0) {
-            const bestMatch = matches.reduce((a, b) => a.length >= b.length ? a : b); // keyの文字数が最も長いものを選ぶ
-            result.push(bestMatch); // 返り値に追加
-            i += bestMatch.length; // マッチした文字数分だけ進む
+    while (i < word.length) {
+        const match = lsystem.filter(key => word.slice(i).startsWith(key));
+        if (match.length > 0) {
+            const best_match = match.reduce((a, b) => a.length >= b.length ? a : b);
+            result.push(best_match);
+            i += best_match.length;
         } else {
-            i++; // マッチしなければ次の文字へ
+            if (!/\s/.test(word[i])) result.push(word[i]);
+            i++;
         }
     }
     return result;
 }
 
 class drawcanvas {
-    // 直交座標→canvas用座標へのスケーリング
-    scaling(point) {
+    scl(point) {
         return [
-            point[0] / graphsize * canvasSize,
-            (1 - point[1] / graphsize) * canvasSize
+            point[0] / init_prm[5] * canvas_size,
+            (1 - point[1] / init_prm[5]) * canvas_size
         ];
     }
-    // 現在の位置から線を引く
     line(point) {
-        const scaled = this.scaling(point);
-        const scaledcurrent = this.scaling(currentPoint);
+        const scaled_point = this.scl(point);
+        const scaled_current_point = this.scl(current_point);
         ctx.beginPath();
-        ctx.moveTo(scaledcurrent[0], scaledcurrent[1]);
-        ctx.lineTo(scaled[0], scaled[1]);
+        ctx.moveTo(scaled_current_point[0], scaled_current_point[1]);
+        ctx.lineTo(scaled_point[0], scaled_point[1]);
         ctx.stroke();
-        currentPoint = point;
+        current_point = point;
     }
-    // 任意の場所に点を打つ
     dot(point) {
-        const scaled = this.scaling(point);
+        const scaled_point = this.scl(point);
         ctx.beginPath();
-        ctx.arc(scaled[0], scaled[1], 5, 0, Math.PI * 2);
+        ctx.arc(scaled_point[0], scaled_point[1], 5, 0, Math.PI * 2);
         ctx.fill();
     }
 }
 
 class Lsystemfunction {
-    // 命令Fの処理
     F() {
         D.line([
-            currentPoint[0] + lineLength * Math.cos(currentAngle),
-            currentPoint[1] + lineLength * Math.sin(currentAngle)
+            current_point[0] + init_prm[4] * Math.cos(current_angle),
+            current_point[1] + init_prm[4] * Math.sin(current_angle)
         ]);
     }
-    // 命令+の処理
-    plus() {
-        currentAngle -= angle;
-    }
-    // 命令-の処理
-    minus() {
-        currentAngle += angle;
-    }
-    // 命令[の処理
-    sbS() {
-        keep = { point: currentPoint, angle: currentAngle };
-    }
-    // 命令]の処理
+    plus() { current_angle -= init_prm[3] / 180 * Math.PI; }
+    minus() { current_angle += init_prm[3] / 180 * Math.PI; }
+    sbS() { keep_list = { point: current_point, angle: current_angle }; }
     sbF() {
-        currentPoint = keep.point;
-        currentAngle = keep.angle;
+        current_point = keep_list.point; current_angle = keep_list.angle;
     }
 }
 
+function resize_canvas() {
+    canvas_size = canvas.getBoundingClientRect().width;
+    canvas.width = canvas_size;
+    canvas.height = canvas_size;
+}
+
+function reset_canvas() {
+    current_point = init_prm.slice(0, 2);
+    current_angle = init_prm[2] / 180 * Math.PI;
+    keep_list = { point: undefined, angle: undefined };
+    ctx.clearRect(0, 0, canvas_size, canvas_size);
+}
+
+teb_ctrl.forEach((ctrl, target) => ctrl.addEventListener(`click`, () => change_tab(target)));
+
+function change_tab(target) {
+    for (let i = 0; i < tab_arr.length; i++) {
+        tab_arr[i].style.display = i == target ? `block` : `none`;
+        teb_ctrl[i].style.zIndex = i == target ? `10` : `0`;
+    }
+}
+
+
+function log_update(tx) {
+    log_area.textContent += `${tx}\n`;
+}
+
+function log_reset() {
+    log_area.textContent = ``;
+    log_area.style.height = `auto`;
+}
+
+function main() {
+    let is_axiom;
+    let is_rule = true;
+    let rule = [];
+    let tmp = [];
+    let axiom = [];
+    for (let el of find(textarea.value)) {
+        if (is_axiom !== false) {
+            if (el === `Axiom:`) {
+                is_axiom = true; continue;
+            };
+            if (el === `;` && is_axiom) is_axiom = false;
+            if (is_axiom) (el === `->` || el === `;`) ? log_update(`不正な形式です|at`) : axiom.push(el);
+        } else {
+            if (el === `->` || el === `;`) {
+                if (el === (is_rule ? `->` : `;`)) {
+                    is_rule = !is_rule;
+                    rule.push(tmp);
+                } else {
+                    log_update(`不正な形式です`);
+                    return;
+                };
+                tmp = [];
+            } else {
+                tmp.push(el);
+            }
+        }
+    };
+    for (let i = 0; i < init_prm[6]; i++) {
+        for (let j = 0; j < (rule.length - rule.length % 2) / 2; j++) {
+            const l_side = rule[2 * j];
+            const r_side = rule[2 * j + 1];
+            if (!l_side.length || !r_side.length) {
+                log_update(`空の変換規則を作ることはできません`);
+                return;
+            };
+            axiom = replace(axiom, l_side, r_side);
+        }
+    }
+    reset_canvas();
+    for (let rule of axiom) {
+        switch (rule) {
+            case `F`: L.F(); break;
+            case `+`: L.plus(); break;
+            case `-`: L.minus(); break;
+            case `[`: L.sbS(); break;
+            case `]`: {
+                if (keep_list.angle === undefined) {
+                    log_update(`エラーメッセージ:\n命令"]"によるデータ復元を実行できません\n先に命令"["でデータを保存する必要があります`);
+                    return;
+                } else {
+                    L.sbF();
+                }
+            }; break;
+        }
+    };
+}
+
+
+// 数値入力欄に入力を検知したら更新する
+input_num_arr.forEach((el, i) => {
+    el.oninput = () => {
+        init_prm[i] = Number(el.value);
+        reset_canvas();
+        main();
+    };
+});
+
 const L = new Lsystemfunction;
 const D = new drawcanvas;
+window.onresize = resize_canvas();
+window.onload = resize_canvas(), change_tab(0), var_init(), reset_canvas();
